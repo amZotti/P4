c95fca1f4b3a34b7791efa1924bbeeb2b0dd1e06
amzotti <azotti1337@gmail.com>
2014-11-21 23:44:04 -0800

    User must login to search or view home link

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,15 +3,19 @@ User.destroy_all
 User.create!([{
   email: "zombifyYourMom@brains.eu",
   location: "Zombie London",
-  url: "http://i.ytimg.com/vi/UB5-Tst0wik/hqdefault.jpg"
+  url: "http://i.ytimg.com/vi/UB5-Tst0wik/hqdefault.jpg",
+  password_digest: "ewfewfewf"
 },
 {
   email: "doesNOTtasteLikeBrains@brains.eu",
   location: "Zombie London",
-  url: "http://i.vimeocdn.com/video/439873851_1280x720.jpg"
+  url: "http://i.vimeocdn.com/video/439873851_1280x720.jpg",
+  password_digest: "r32r2fffff"
+
 },
 {
   email: "EuroZomb521@brains.eu",
   location: "Zombie Europe",
-  url: "https://zombiegames01.files.wordpress.com/2010/11/dead_state_render_18.jpg"
+  url: "https://zombiegames01.files.wordpress.com/2010/11/dead_state_render_18.jpg",
+  password_digest: "3f32f32f32r3232"
 }])
